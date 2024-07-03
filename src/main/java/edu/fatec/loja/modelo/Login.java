ac919d57232b65692d9af609d858da3f79bb15be
marikm <matsumotomarina3@gmail.com>
2024-07-03 10:23:35 -0300

    testes para classe Login com exceções testadas

--- src/main/java/edu/fatec/loja/modelo/Login.java
+++ src/main/java/edu/fatec/loja/modelo/Login.java
@@ -1,20 +1,18 @@
 package edu.fatec.loja.modelo;
 
-public class Login {
-    private final String nomeUsuario;
-    private final String senha;
-
+public record Login(String nomeUsuario, String senha) {
     public Login(String nomeUsuario, String senha) {
-        this.nomeUsuario = nomeUsuario;
-        this.senha = senha;
-    }
 
-    public String getNomeUsuario() {
-        return nomeUsuario;
-    }
+        if (nomeUsuario == null || nomeUsuario.trim().isEmpty()) {
+            throw new IllegalArgumentException("Nome do usuário não pode ser nulo nem vazio");
+        }
 
-    public String getSenha() {
-        return senha;
+        if (senha == null || senha.length() < 5) {
+            throw new IllegalArgumentException("Campo senha não pode ser nulo nem conter menos de 5 caracteres");
+        }
+
+        this.nomeUsuario = nomeUsuario.trim();
+        this.senha = senha.trim();
     }
 
     @Override
--- src/main/java/edu/fatec/loja/modelo/Usuario.java
+++ src/main/java/edu/fatec/loja/modelo/Usuario.java
@@ -1,5 +1,7 @@
 package edu.fatec.loja.modelo;
 
+import java.util.logging.Logger;
+
 public class Usuario {
 
 }
