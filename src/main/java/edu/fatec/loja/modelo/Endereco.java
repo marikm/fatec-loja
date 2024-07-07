00e42cc5b30c071d707d44403da33e5f4d39f9c9
marikm <matsumotomarina3@gmail.com>
2024-07-07 20:44:45 -0300

    criado a classe Endereco do Usuario

---
+++ src/main/java/edu/fatec/loja/modelo/Endereco.java
@@ -0,0 +1,43 @@
+package edu.fatec.loja.modelo;
+
+public class Endereco {
+    private final String cidade;
+    private final String rua;
+    private final String numeroCasa;
+
+    public Endereco(String cidade, String rua, String numeroCasa) {
+        if (cidade == null || cidade.isEmpty()) {
+            throw new IllegalArgumentException("Por favor, digite o nome da cidade");
+        }
+        if (rua == null || rua.isEmpty()) {
+            throw new IllegalArgumentException("Por favor, digite o nome da rua");
+        }
+        if (numeroCasa == null || numeroCasa.isEmpty()) {
+            throw new IllegalArgumentException("Por favor, digite o numero da casa");
+        }
+
+        this.cidade = cidade;
+        this.rua = rua;
+        this.numeroCasa = numeroCasa;
+    }
+
+    @Override
+    public String toString() {
+        return "Endereco{" +
+                "cidade='" + cidade + '\'' +
+                ", rua='" + rua + '\'' +
+                ", numeroCasa='" + numeroCasa + '\'' +
+                '}';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/edu/fatec/loja/modelo/LoginTest.java
+++ src/test/java/edu/fatec/loja/modelo/LoginTest.java
@@ -1,23 +1,34 @@
 package edu.fatec.loja.modelo;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class LoginTest {
 
+
+    final Login login = new Login("marina", "12345");
+
     @Test
     public void verificaGettersEstaoCorretos() {
-        final Login login = new Login("marina", "123");
+        assertEquals("marina", login.nomeUsuario());
+        assertEquals("12345", login.senha());
+    }
+    @Test
+    public void verificaExcecoes(){
+        assertDoesNotThrow(() -> login.nomeUsuario());
+        assertDoesNotThrow(() -> login.senha());
 
-        assertEquals("marina", login.getNomeUsuario());
-        assertEquals("123", login.getSenha());
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> new Login("", ""));
+
+
     }
 
 
     @Test
     void testToString() {
-        assertTrue(false);
-        //TODO
+        assertEquals("Login{senha='12345', nomeUsuario='marina'}", login.toString());
     }
 
 }
